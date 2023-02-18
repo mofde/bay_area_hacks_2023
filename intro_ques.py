f15855cc0c1c1278ea84b3e16a0b58c0004794fd
Delphindae99 <94207752+Delphindae99@users.noreply.github.com>
2023-02-18 15:53:06 -0500

    base code

--- intro_ques.py
+++ intro_ques.py
@@ -1,19 +1,22 @@
 #the survey questions
 
 #variable
-i = 0
 
 #ask input
-input "How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert"
+i = input("How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
 
 #the grouping
 #make all categories as functions to be used later, these need to store the data of people
-if i=i:
+if i=="i":
     introvert()
-elif i=a:
+elif i=="a":
     ambivert()
-elif i=e:
+elif i=="e":
     extrovert()
 else:
     #prompt to answer correctly
+<<<<<<< HEAD
+    
+=======
     prompt()
+>>>>>>> 5b99708899c662ec5f055dadb6d5798417f507b9
