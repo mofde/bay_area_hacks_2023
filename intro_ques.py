b8f030adb5bc6385fef627ead2a500a18f63b1f7
microwaved_child <68186963+Cra2yAJ@users.noreply.github.com>
2023-02-19 02:08:44 +0530

    .

--- intro_ques.py
+++ intro_ques.py
@@ -2,3 +2,14 @@
 
 i = 0
 input "How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert"
+
+#the grouping
+
+if i=i:
+    introvert()
+elif i=a:
+    ambivert()
+elif i=e:
+    extrovert()
+else:
+    #prompt to answer correctly
