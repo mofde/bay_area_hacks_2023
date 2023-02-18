68fc5919bb982503a66310ee220fb37140dfd52f
microwaved_child <68186963+Cra2yAJ@users.noreply.github.com>
2023-02-19 02:11:01 +0530

    .

--- intro_ques.py
+++ intro_ques.py
@@ -1,10 +1,13 @@
 #the survey questions
 
+#variable
 i = 0
+
+#ask input
 input "How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert"
 
 #the grouping
-
+#make all categories as functions to be used later
 if i=i:
     introvert()
 elif i=a:
