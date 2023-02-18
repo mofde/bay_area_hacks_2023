7d56e49f67d65baf64b937aab2ce39ad261f1a20
Delphindae99 <94207752+Delphindae99@users.noreply.github.com>
2023-02-18 16:33:30 -0500

    function

--- intro_ques.py
+++ intro_ques.py
@@ -3,19 +3,20 @@
 #variable
 
 #ask input
-i = input("How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
+def ask():
+    i = input("How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
 
-#the grouping
-#make all categories as functions to be used later, these need to store the data of people
-if i=="i":
-    introvert()
-elif i=="a":
-    ambivert()
-elif i=="e":
-    extrovert()
-else:
-    #prompt to answer correctly
-    prompt()
+    #the grouping
+    #make all categories as functions to be used later, these need to store the data of people
+    if i=="i":
+        introvert()
+    elif i=="a":
+        ambivert()
+    elif i=="e":
+        extrovert()
+    else:
+        #prompt to answer correctly
+        prompt()
 
 def prompt():
     i = input("Invalid input! How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
