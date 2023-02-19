1a694235b1c19659bfab1062657488fd8e1d391b
Delphindae99 <94207752+Delphindae99@users.noreply.github.com>
2023-02-18 21:50:20 -0500

    Added a class to intro_ques.py

--- intro_ques.py
+++ intro_ques.py
@@ -3,7 +3,17 @@
 #variable
 
 #ask input
-def ask():
+
+class Person(object):
+    #constructor
+    def _init_(self):
+        self._sociality = input("How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
+        self._description = input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful")
+        self._hobby = input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology")
+        self._landscape = input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains")
+        self._movie = input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama")
+
+"""def ask():
     i = input("How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
 
     #the grouping
@@ -16,22 +26,13 @@ def ask():
         extrovert()
     else:
         #prompt to answer correctly
-        prompt()
+        prompt()"""
 
-def prompt():
+"""def prompt():
     i = input("Invalid input! How would you describe yourself?     (i)Introvert    (a)Ambivert     (e)Extrovert")
     if i=="i":
         introvert()
     elif i=="a":
         ambivert()
     elif i=="e":
-        extrovert()
-
-def introvert():
-    #placeholder
-
-def ambivert():
-    #placeholder
-
-def extrovert():
-    #placeholder
+        extrovert()"""
