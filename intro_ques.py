e5586ebaac1ecf8db29e85e9a6d3924472d13fd0
Delphindae99 <94207752+Delphindae99@users.noreply.github.com>
2023-02-19 10:38:34 -0500

    functions to sort out meaning of input questions

--- intro_ques.py
+++ intro_ques.py
@@ -7,27 +7,29 @@
 class introvert(object):
     #constructor
     def _init_(self):
-        self._description = input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful")
-        self._hobby = input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology")
-        self._landscape = input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains")
-        self._movie = input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama")
+        self._description = findDescription(input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful"))
+        self._hobby = findHobby(input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology"))
+        self._landscape = findLandscape(input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains"))
+        self._movie = findMovie(input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama"))
+
+
     
 
 class ambivert(object):
     #constructor
     def _init_(self):
-        self._description = input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful")
-        self._hobby = input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology")
-        self._landscape = input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains")
-        self._movie = input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama")
+        self._description = findDescription(input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful"))
+        self._hobby = findHobby(input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology"))
+        self._landscape = findLandscape(input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains"))
+        self._movie = findMovie(input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama"))
     
 class extrovert(object):
     #constructor
     def _init_(self):
-        self._description = input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful")
-        self._hobby = input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology")
-        self._landscape = input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains")
-        self._movie = input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama")
+        self._description = findDescription(input("How would you describe yourself? (c)Creative (s)Smart (e)Easygoing (i)Insightful"))
+        self._hobby = findHobby(input("What do you prefer to do in your spare time? (v)Video games (c)Creative activities (s)Sports (t)Technology"))
+        self._landscape = findLandscape(input("What kind of landscape do you prefer? (f)Forest (c)City (o)Ocean (m)Mountains"))
+        self._movie = findMovie(input("What kinds of movie do you prefer? (a)Action (r)Romance (c)Comedy (d)Drama"))
 
 
 def ask():
@@ -53,3 +55,47 @@ def prompt():
         newPerson = ambivert()
     elif i=="e":
          newPerson = extrovert()
+
+def findDescription(description):
+    description = description.lower()
+    if description == "c":
+        return "creative"
+    elif description == "s":
+        return "smart"
+    elif description == "e":
+        return "easygoing"
+    elif description == "i":
+        return "insightful"
+
+def findHobby(hobby):
+    hobby = hobby.lower()
+    if hobby == "v":
+        return "video games"
+    elif hobby == "c":
+        return "creative activities"
+    elif hobby == "s":
+        return "sports"
+    elif hobby == "t":
+        return technology
+
+def findLandscape(landscape):
+    landscape = landscape.lower()
+    if landscape == "f":
+        return "forest"
+    if landscape == "o":
+        return "ocean"
+    if landscape == "c":
+        return "city"
+    if landscape == "m":
+        return "mountains"
+
+def findMovie(movie):
+    movie = movie.lower()
+    if movie == "a":
+        return "action"
+    elif movie == "r":
+        return "romance"
+    elif movie == "c":
+        return "comedy"
+    elif movie == "d":
+        return "drama"
