6c6c723026825e06386c977a5758236f14e679e6
Melissa Gatti <mofde@udel.edu>
2023-02-19 18:19:33 -0500

    Added in usernameInput and passwordInput, but still need to find out if the usernames and passwords correlate

--- login.py
+++ login.py
@@ -1,10 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == "__main__":
-    app.run()
+import maskpass
+usernameInput = input("Username: ")
+passwordInput = maskpass.askpass(prompt = "Password: ", mask = "*")
