54c6ccf182fe44e63cb93aa44179c546467f4417
Delphindae99 <94207752+Delphindae99@users.noreply.github.com>
2023-02-20 09:29:10 -0500

    login.py - removed main

--- login.py
+++ login.py
@@ -1,39 +1,28 @@
 import maskpass
 import csv
 from csv import writer
+def login() -> int:
+    login = 0
+    while login == 0:
+        usernameInput = input("Username: ")
+        passwordInput = maskpass.askpass(prompt = "Password: ", mask = "")
+        with open("usernames_and_passwords.csv", 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                if usernameInput == row["Username"] and passwordInput == row["Password"]:
+                    return 1
+            print("Username or password is incorrect")
 
-startQuestions = False
-
-newline = "\n"
-def login():
-    usernameInput = input("Username: ")
-    passwordInput = maskpass.askpass(prompt = "Password: ", mask = "*")
-    with open("usernames_and_passwords.csv", 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            if usernameInput == row["Username"] and passwordInput == row["Password"]:
-                startQuestions = True
-                return 1
-        print("Username or password is incorrect")
-
-def createNewAccount():
-    newAccountCreated = 0;
+def createNewAccount() -> int:
+    newAccountCreated = 0
     while newAccountCreated == 0:
         usernameInput = input("Username: ")
-        passwordInput = maskpass.askpass(prompt = "Password: ", mask = "*")
+        passwordInput = maskpass.askpass(prompt = "Password: ", mask = "")
         confirmPasswordInput = maskpass.askpass(prompt = "Confirm Password: ", mask = "*")
         if passwordInput == confirmPasswordInput:
-            newAccountCreated = 1;
+            newAccountCreated = 1
     with open("usernames_and_passwords.csv", 'a', newline = '') as csv_file:
         writer_object = writer(csv_file)
         writer_object.writerow([usernameInput, passwordInput])
         csv_file.close()
-        startQuestions = True
-        return 1
-
-if __name__ == "__main__":
-    accountInput = input("Do you currently have an account? ")
-    if accountInput.lower() == "yes":
-        login()
-    else:
-        createNewAccount()
+    return 1
