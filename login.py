45c2e6d42f081dd49e8a944ef000deb1100c721d
Melissa Gatti <mofde@udel.edu>
2023-02-19 21:31:05 -0500

    Created a list of usernames and passwords to make sure logining in and out works

--- login.py
+++ login.py
@@ -1,3 +1,14 @@
 import maskpass
-usernameInput = input("Username: ")
-passwordInput = maskpass.askpass(prompt = "Password: ", mask = "*")
+import csv
+def login():
+    usernameInput = input("Username: ")
+    passwordInput = maskpass.askpass(prompt = "Password: ", mask = "*")
+    with open("usernames_and_passwords.csv") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if usernameInput == row["Username"] and passwordInput == row["Password"]:
+                return
+        print("Username or password is incorrect")
+
+if __name__ == "__main__":
+    login()
